26e028d8f388779adf34058ed9f26c041761f0b9
obdsf <Geo.Giotopoulos@gmail.com>
2019-05-14 20:51:55 +0300

    Update Diceobu.cpp

--- Diceobu/Diceobu.cpp
+++ Diceobu/Diceobu.cpp
@@ -5,4 +5,6 @@ int main()
 	std::cout << "Welcome to Dfireobu also known as Diceobu Jr, or Diceobu's younger sibling.\n"
 		<< "Dfireobu is nothing like Diceobu.\n"
 		<< "It's just a platform that will guide you through the basic features Diceobu will have to offer.\n";
+
+	return 0;
 }
