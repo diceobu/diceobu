6623d8aa249d00afc52e869a881cc2fc2fb5ae01
obdsf <Geo.Giotopoulos@gmail.com>
2019-05-14 19:52:37 +0300

    Update Diceobu.cpp

--- Diceobu/Diceobu.cpp
+++ Diceobu/Diceobu.cpp
@@ -3,6 +3,6 @@
 int main()
 {
     std::cout << "Welcome to the Diceobu features test.\n"
-		<< "This is not the complete game and is nothing like the complete version of the game.\n"
-		<< "This is only a platform that helps you explore the existing features of the game.\n";
+		<< "This is not the complete game.\n"
+		<< "This is only a platform that helps you explore the current features of the game.\n";
 }
