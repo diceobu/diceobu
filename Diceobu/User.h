937f657e9741e25b0e530d1b61d0a022282061d5
Denshin <d3cepti0n@hotmail.com>
2019-05-17 15:20:36 +0300

    userfinal

--- Diceobu/User.h
+++ Diceobu/User.h
@@ -34,9 +34,34 @@ private:
 //	Member Functions
 public:
 //	Constructors
-	User();
+	User(	const int			&userID,			const std::string	&displayName,
+			const std::string	&password,			const std::string	&email,
+			const std::string	&listOfCharacters,	const std::string	&ongoingCamps,
+			const bool			&isDungeonMaster,	const std::string	&prefferences,
+			const std::string	&friendList,		const std::string	&gameHistory);
 //	Access Functions
 //	Getters
+	const int			getUserID()				{ return m_userID; }
+	const std::string	getDisplayName()		{ return m_displayName; }
+	const std::string	getPassword()			{ return m_password; }
+	const std::string	getEmail()				{ return m_email; }
+	const std::string	getListOfCharacters()	{ return m_listOfCharacters; }
+	const std::string	getOngoingCamps()		{ return m_ongoingCamps; }
+	const bool			getIsDungeonMaster()	{ return m_isDungeonMaster; }
+	const std::string	getPrefferences()		{ return m_prefferences; }
+	const std::string	getFriendList()			{ return m_friendList; }
+	const std::string	getGameHistory()		{ return m_gameHistory; }
+
 //	Setters
+	void	setUserID(const int &userID)								{ m_userID = userID; }
+	void	setDisplayName(const std::string &displayName)				{ m_displayName = displayName; }
+	void	setPassword(const std::string &password)					{ m_password = password; }
+	void	setEmail(const std::string &email)							{ m_email = email; }
+	void	setListOfCharacters(const std::string &listOfCharacters)	{ m_listOfCharacters = listOfCharacters; }
+	void	setOngoingCamps(const std::string &ongoingCamps)			{ m_ongoingCamps = ongoingCamps; }
+	void	setIsDungeonMaster(const bool	&isDungeonMaster)			{ m_isDungeonMaster = isDungeonMaster; }
+	void	setPrefferences(const std::string &prefferences)			{ m_prefferences = prefferences; }
+	void	setFriendList(const  std::string &friendList)				{ m_friendList = friendList; }
+	void	setGameHistory(const  std::string &gameHistory)				{ m_gameHistory = gameHistory; }
 //	Others
 };
