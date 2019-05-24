c7a6b7c94026cd750917ccd52a9efd41dc9d786a
obdsf <Geo.Giotopoulos@gmail.com>
2019-05-24 20:22:04 +0300

    nothing really

--- Diceobu/GameData.h
+++ Diceobu/GameData.h
@@ -27,40 +27,57 @@ std::array<std::array<tileData, mapSize>, mapSize> tileDataContainer;
 //	Data Functions
 void initTileDataCont()
 {
-	//	tile (row, column).
-	//	tile (5, 5).
-	tileDataContainer[5][5].tld_terrainType = "dirt";
-	tileDataContainer[5][5].tld_open = true;
-	//	tile (5, 6).
-	tileDataContainer[5][6].tld_terrainType = "dirt";
-	tileDataContainer[5][6].tld_open = true;
-	//	tile (6, 5).
-	tileDataContainer[6][5].tld_terrainType = "dirt";
-	tileDataContainer[6][5].tld_open = true;
-	//	tile (6, 6).
-	tileDataContainer[6][6].tld_terrainType = "dirt";
-	tileDataContainer[6][6].tld_open = true;
-	//	tile(30, 30).
-	tileDataContainer[30][30].tld_terrainType = "dirt";
-	tileDataContainer[30][30].tld_open = true;
-	//	tile(30, 31).
-	tileDataContainer[30][31].tld_terrainType = "dirt";
-	tileDataContainer[30][31].tld_open = true;
-	//	tile(31, 30).
-	tileDataContainer[31][30].tld_terrainType = "dirt";
-	tileDataContainer[31][30].tld_open = true;
-	//	tile(31, 31).
-	tileDataContainer[31][31].tld_terrainType = "dirt";
-	tileDataContainer[31][31].tld_open = true;
-	//	tile(30, 32).
-	tileDataContainer[30][32].tld_terrainType = "dirt";
-	tileDataContainer[30][32].tld_open = true;
-	//	tile(31, 32).
-	tileDataContainer[31][32].tld_terrainType = "dirt";
-	tileDataContainer[31][32].tld_open = true;
-	//	tile(33, 33).
-	tileDataContainer[33][33].tld_terrainType = "dirt";
-	tileDataContainer[33][33].tld_open = true;
+	int my_var1 = 17;
+	int my_var2 = 21;
+	for (int i = 11; i <= 39; i++)
+	{
+		for (int j = 11; j <= 39; j++)
+		{
+			if ((j == 16) && (i == 16) || (j == 16) && (i == 20))
+			{
+				tileDataContainer[i][j].tld_terrainType = "stone";
+				tileDataContainer[i][j].tld_open = true;
+			}
+			if ((i >= my_var1) && (i <= 23))
+			{
+				tileDataContainer[my_var1][my_var1].tld_terrainType = "stone";
+				tileDataContainer[my_var1][my_var1].tld_open = true;
+				tileDataContainer[my_var1][my_var1 - 1].tld_terrainType = "stone";
+				tileDataContainer[my_var1][my_var1 - 1].tld_open = true;
+				my_var1++;
+			}
+			if ((i >= my_var2) && (i <= 24))
+			{
+				if (i == 24)
+				{
+					tileDataContainer[my_var2][my_var2 - 5].tld_terrainType = "stone";
+					tileDataContainer[my_var2][my_var2 - 5].tld_open = true;
+					tileDataContainer[my_var2][my_var2 - 4].tld_terrainType = "stone";
+					tileDataContainer[my_var2][my_var2 - 4].tld_open = true;
+					my_var2++;
+				}
+				else
+				{
+					tileDataContainer[my_var2][my_var2 - 5].tld_terrainType = "stone";
+					tileDataContainer[my_var2][my_var2 - 5].tld_open = true;
+				}
+			}
+			if ((i >= 23) && (i <= 27) && (j == 23) || (i == 27) && (j >= 23) && (j <= 27)
+				|| ((i >= 24) && (i <= 30) && (j == 20)) || ((i == 30) && (j >= 20) && (j <= 26))
+				|| (i == 23) && (j >= 27) && (j <= 50) || (i >= 23) && (i <= 34) && (j == 39)
+				|| (i == 34) && (j >= 27) && (j <= 50)
+				|| ((j == 27) && (i >= 23) && (i <= 27) || (j == 27) && (i >= 30) && (i <= 34)))
+			{
+				tileDataContainer[i][j].tld_terrainType = "stone";
+				tileDataContainer[i][j].tld_open = true;
+			}
+			if (tileDataContainer[i][j].tld_terrainType == "none")
+			{
+				tileDataContainer[i][j].tld_terrainType = "dirt";
+				tileDataContainer[i][j].tld_open = true;
+			}
+		}
+	}
 }
 
 void initAccountDetails()
